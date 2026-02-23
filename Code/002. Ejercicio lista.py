276615b18aa4002622a248b0160fcc187bcc8231
Chamuzo <github.clustered634@passmail.net>
2026-02-23 13:36:59 +0100

    AA

--- Code/002. Ejercicio lista.py
+++ Code/002. Ejercicio lista.py
@@ -8,16 +8,24 @@ def generar_lista(num):
     lista = []
     x = num
     while x > 0 in range(0,num):
-        if x%2 != 0:
+        if x % 2 != 0:
             lista.append(x)
         x -= 1
     return lista
-print(generar_lista(10))
+
+print(f"Listado de números impares: ", end="")
+for i in generar_lista(21):
+    print(i, end="|")
+print()
 
 #Version For
 def generar_lista2(num):
     lista = []
     for i in range (0,num+1):
-        if i%2 != 0:
+        if i % 2 != 0:
             lista.append(i)
-print(generar_lista2(10))
+    return lista
+print(f"Listado de números impares: ", end="")
+for i in generar_lista2(21):
+    print(i, end="|")
+
