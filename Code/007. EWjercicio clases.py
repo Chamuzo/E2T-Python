e51d4a4fcdfb234299969106dedee0c397efa085
Chamuzo <github.clustered634@passmail.net>
2026-02-25 13:06:38 +0100

    a

---
+++ Code/007. EWjercicio clases.py
@@ -0,0 +1,22 @@
+"""
+class Persona:
+    def __init__(self,name,age,rank):
+        self.nombre = name
+        self.edad = age
+        self.empleo = rank
+
+
+    def correr(self):
+        print(f"{self.nombre} está corriendo")
+
+    def cumpleaños(self):
+        self.edad += 1
+        print(self.edad)
+
+antonio = Persona("Antonio", 36,"Sgt1")
+ruben = Persona ("Ruben",44,"Sgt1")
+antonio.cumpleaños()
+ruben.cumpleaños()
+ruben.cumpleaños()
+ruben.cumpleaños()
+"""
