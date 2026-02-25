aafa2c9a8e9ed58c7cde9df5388abfab38564474
Chamuzo <github.clustered634@passmail.net>
2026-02-25 09:41:11 +0100

    a

--- Code/005. Ejercicio num_aleatorio().py
+++ Code/005. Ejercicio num_aleatorio().py
@@ -6,7 +6,7 @@ max = 100 #rango máximo
 for i in range (1,max+1):
     rango.append(i)
 rand_num = random.choice(rango)
-#ESTO MISMO SE PUEDE HACER CON RANDOM.RANDINT
+#ESTO MISMO SE PUEDE HACER CON RANDOM.RANDINT (1,100)
 intentos = 5
 while intentos > 0:
     choice = int(input(f"Seleciona un número del 1 al {max}. \n Dispones de {intentos} intentos. \n Número: "))
